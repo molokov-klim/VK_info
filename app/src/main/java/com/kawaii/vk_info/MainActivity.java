925a2b9b9e96570c2f8a00435e1ba67c3c100107
Molokov-Klim <ultrakawaii9654449192@gmail.com>
2022-01-24 16:58:56 +0300

    UI done

--- app/src/main/java/com/kawaii/vk_info/MainActivity.java
+++ app/src/main/java/com/kawaii/vk_info/MainActivity.java
@@ -3,13 +3,23 @@ package com.kawaii.vk_info;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText searchField;
+    private Button searchButton;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchField = findViewById(R.id.et_search_field);
+        searchButton = findViewById(R.id.b_search_vk);
+        result = findViewById(R.id.tv_result);
     }
 }
 
