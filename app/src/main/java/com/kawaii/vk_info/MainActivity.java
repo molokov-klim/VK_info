ab1e4f061064c0c2741c0cf308b270d18b0b22c0
Molokov-Klim <ultrakawaii9654449192@gmail.com>
2022-01-26 17:44:21 +0300

    parsing response added

--- app/src/main/java/com/kawaii/vk_info/MainActivity.java
+++ app/src/main/java/com/kawaii/vk_info/MainActivity.java
@@ -5,11 +5,16 @@ import static com.kawaii.vk_info.utils.NetworkUtils.getResponseFromURL;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.URL;
@@ -19,6 +24,49 @@ public class MainActivity extends AppCompatActivity {
     private Button searchButton;
     private TextView result;
 
+    //создаем класс для потока. AsyncTask<что отправляем, что получаем в процессе, что получаем в итоге>
+    class VKQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        // метод ожидает массив из URL
+        protected String doInBackground(URL... urls) {
+            String response = null;
+            try {
+                response = getResponseFromURL(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return response;
+        }
+        // вставляем ответ в элемент
+        @Override
+        protected void onPostExecute(String response){
+            String firstName = null;
+            String  lastName = null;
+            //парсим JSON "{response:[{"id":123,"first_name":"Klim","last_name":"Molokov"}]}
+            try {
+                JSONObject jsonResponse = new JSONObject(response);
+                // это массив в json'е
+                JSONArray jsonArray = jsonResponse.getJSONArray(response);
+                JSONObject userInfo = jsonArray.getJSONObject(0);
+
+                firstName = userInfo.getString("first_name");
+                lastName = userInfo.getString("last_name");
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            String resultingString = "Имя: " + firstName + "\n" + "Фамилия: " + lastName;
+            result.setText(resultingString);
+            System.out.print(result);
+            System.out.print(firstName);
+            System.out.print(lastName);
+        }
+    }
+
+    // главный поток
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,13 +81,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 URL generatedURL = generateURL(searchField.getText().toString());
 
-                String response = null;
-                try {
-                    response = getResponseFromURL(generatedURL);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                result.setText(response);
+                new VKQueryTask().execute(generatedURL);
+
 
             }
         };
--- app/src/main/java/com/kawaii/vk_info/utils/NetworkUtils.java
+++ app/src/main/java/com/kawaii/vk_info/utils/NetworkUtils.java
@@ -15,12 +15,15 @@ public class NetworkUtils {
     private static final String VK_USERS_GET = "/method/users.get";
     private static final String PARAM_USER_ID = "user_ids";
     private static final String PARAM_VERSION = "v";
+    private static final String ACCESS_TOKEN = "access_token";
 
+    // формирование url запроса
     public static URL generateURL(String userID) {
         Uri builtUri = Uri.parse(VK_API_BASE_URL + VK_USERS_GET)
                 .buildUpon()
                 .appendQueryParameter(PARAM_USER_ID, userID)
-                .appendQueryParameter(PARAM_VERSION, "5.8")
+                .appendQueryParameter(PARAM_VERSION, "5.81")
+                .appendQueryParameter(ACCESS_TOKEN, "09c0367109c0367109c03671d309bb34f2009c009c0367168300e9b406bb1b3aaee0793")
                 .build();
 
         URL url = null;
@@ -33,6 +36,7 @@ public class NetworkUtils {
         return url;
     }
 
+    // it's street magic
     public static String getResponseFromURL(URL url) throws IOException {
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
@@ -40,6 +44,7 @@ public class NetworkUtils {
             InputStream in = urlConnection.getInputStream();
 
             Scanner scanner = new Scanner(in);
+            // задание разделения по переходу на новую строку
             scanner.useDelimiter("\\A");
 
             boolean hasInput = scanner.hasNext();
